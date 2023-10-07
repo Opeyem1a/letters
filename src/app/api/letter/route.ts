a184b58c21e74ab76a24ce0dde8d00ab3187eb76
opeyem1a <opeyadeyemi@gmail.com>
2023-10-07 03:57:56 -0600

    add create and list api routes

--- src/app/api/letter/route.ts
+++ src/app/api/letter/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server"
 import { db } from "@/db"
 import { eq } from "drizzle-orm"
 import { letters } from "@db/schema"
-import { MaybeLetter } from "@db/schema/types"
-import {DrizzleErrors, errorResponse} from "@/utils/api";
+import { MaybeLetter, NewLetter } from "@db/schema/types"
+import { DrizzleErrors, errorResponse } from "@/utils/api"
 
 export const GET = async (request: Request) => {
     const { searchParams } = new URL(request.url)
@@ -27,7 +27,7 @@ export const GET = async (request: Request) => {
                             columns: {
                                 id: false,
                                 name: true,
-                            }
+                            },
                         },
                     },
                 },
@@ -35,13 +35,41 @@ export const GET = async (request: Request) => {
             },
         })
         if (!letter) {
-            return errorResponse(404, `Letter with UUID: ${uuid} could not be found.`)
+            return errorResponse(
+                404,
+                `Letter with UUID: ${uuid} could not be found.`
+            )
         }
         return NextResponse.json(letter)
-    } catch(e) {
-        if(e.code === DrizzleErrors.BAD_UUID) {
+    } catch (e) {
+        if (e.code === DrizzleErrors.BAD_UUID) {
             return errorResponse(500, "Malformed UUID.")
         }
-        return errorResponse(500, "Unknown error occurred. Please try again later.")
+        return errorResponse(
+            500,
+            "Unknown error occurred. Please try again later."
+        )
+    }
+}
+
+export const POST = async (request: Request) => {
+    const body = await request.json()
+
+    if (!body) {
+        return errorResponse(404, "No letter data provided.")
+    }
+
+    try {
+        // todo: add validation for form data
+        //@ts-expect-error
+        const letter: MaybeLetter = await db
+            .insert(letters)
+            .values(body as NewLetter)
+        return NextResponse.json(letter)
+    } catch (e) {
+        return errorResponse(
+            500,
+            "Unknown error occurred. Please try again later." + e
+        )
     }
 }
