55b0c37c782b9decddfe0ffdb26eacb113547908
Emily <emyudesigns@gmail.com>
2023-10-15 10:15:14 +1100

    test privacy page updates

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,11 +1,68 @@
+import "./samplestylesheet.css"
 export default function PrivacyPolicy() {
     return (
         <main>
+
+            <h1>
             Privacy Policy
+            </h1>
             <p>
-                By checking this box, you consent to provide LettersTo with
-                permission to use your letter in any form. For more on our
-                privacy policy, click here.
+                This page informs you on how we collect, use and store data when you use the service, LettersTo.
+
+                When the user has ticked the privacy policy consent box, they grant permission for LettersTo publicly publish the information they have given, inclusive of the personal data they have chosen to provided. Hence, it’s important for you to double check the content you’ve decided to provide, ensuring that it does not harm you or anyone else.
+            </p>
+
+            <h2>
+                Definitions:
+            </h2>
+            <h3>
+                Service
+            </h3>
+            <p>
+                Service refer to LettersTo. At this stage, LettersTo is a passion project/ non-monetised. Should this status change, an update will be announced on both the website as well as social media pages.
+            </p>
+            <h3>
+                Personal Data
+            </h3>
+            <p>
+                Personal data refers to the age, country and content they submit to the service. LettersTo does not collect emails and names as all users’ names are by default, anonymous. See retention of data below to see how your data is handled.
+            </p>
+            <h3>
+                User
+            </h3>
+            <p>
+                User refers to the party that submits their personal data and letters to LettersTo.
+            </p>
+            <h3>
+                Content
+            </h3>
+            <p>
+                Content refers to the letters user wishes to submit to LettersTo.
+            </p>
+            <h3>
+                Social media
+            </h3>
+            <p>
+                Social media refers to platforms not exclusive to Instagram, Youtube, Tiktok, Facebook, Twitter. At this stage, LettersTo only publishes content on Tiktok and Instagram.
+            </p>
+            <h2>
+                Data storage policy:
+            </h2>
+            <h3>
+                Retention of Data
+            </h3>
+            <p>
+                LettersTo will retain your personal data for as long as we see fit(could probs word this nicer). As LettersTo does not collect emails and names, the service cannot retrieve letters upon request.
+
+                Transfer of Data (moreso social media content)
+                When the user has ticked the privacy policy consent box, they grant permission for LettersTo publish the information they have submitted. Once content is published to any social media platform, how the content is handled is dependent on each individual platform’s policies and the relevant nation/state laws & regulations.
+
+                LettersTo does not distribute personal data from the user to any third parties outside of the software/programs used to develop this.
+
+                Have any questions, feedback, thoughts? Feel free to reach out to the team at EMAIL ADDRESS .
+                We endeavour to respond to queries within a week’s time, keeping in mind that the LettersTo team is small.
+
+                [Page last updated: Day, Month as a word, Year?]
             </p>
         </main>
     )
